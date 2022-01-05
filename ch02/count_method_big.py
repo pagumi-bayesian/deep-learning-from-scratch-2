9f17595aa1c302e9f621b7e581de4ae3ee795c63
pagumi-bayesian <d.ogawa2323@gmail.com>
2022-01-05 16:17:55 +0000

    Run ch02

--- ch02/count_method_big.py
+++ ch02/count_method_big.py
@@ -1,3 +1,4 @@
+# %%
 # coding: utf-8
 import sys
 sys.path.append('..')
@@ -5,10 +6,11 @@ import numpy as np
 from common.util import most_similar, create_co_matrix, ppmi
 from dataset import ptb
 
-
+# %%
 window_size = 2
 wordvec_size = 100
 
+# %%
 corpus, word_to_id, id_to_word = ptb.load_data('train')
 vocab_size = len(word_to_id)
 print('counting  co-occurrence ...')
@@ -16,18 +18,23 @@ C = create_co_matrix(corpus, vocab_size, window_size)
 print('calculating PPMI ...')
 W = ppmi(C, verbose=True)
 
+# %%
 print('calculating SVD ...')
 try:
     # truncated SVD (fast!)
     from sklearn.utils.extmath import randomized_svd
     U, S, V = randomized_svd(W, n_components=wordvec_size, n_iter=5,
-                             random_state=None)
+                             random_state=123)
 except ImportError:
     # SVD (slow)
     U, S, V = np.linalg.svd(W)
 
+# %%
 word_vecs = U[:, :wordvec_size]
 
+# %%
 querys = ['you', 'year', 'car', 'toyota']
 for query in querys:
     most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
+
+# %%
